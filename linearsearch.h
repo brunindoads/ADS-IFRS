db0a561721b848732c5cf6d74c003b1632638d07
brunindoads <102702435+brunindoads@users.noreply.github.com>
2023-07-09 12:36:33 -0300

    Delete linearsearch.h

--- linearsearch.h
+++
@@ -1,8 +0,0 @@
-#ifndef LINEARSEARCH_H
-#define LINEARSEARCH_H
-
-// Linear Search.
-int linearSearch(int data[], int size, int value);
-void timeLinearSearch(int data[], int size, int value, int data_set);
-
-#endif
