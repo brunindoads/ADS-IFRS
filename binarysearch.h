cea74638c517d536bbf59e8666f56ddc83d33eb4
brunindoads <102702435+brunindoads@users.noreply.github.com>
2023-07-09 12:34:14 -0300

    Delete binarysearch.h

--- binarysearch.h
+++
@@ -1,8 +0,0 @@
-#ifndef BINARYSEARCH_H
-#define BINARYSEARCH_H
-
-// Binary Search.
-int binarySearch(int data[], int size, int value);
-void timeBinarySearch(int data[], int size, int value, int data_set);
-
-#endif
